23d54e48e9ce24f4c705842f83155fc248bb084e
� Simone <simonetedesco.dev@gmail.com>
2020-12-30 00:47:51 +0100

    updated social icons

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import ContactForm from "./ContactForm";
 
+const list = [
+  {
+    name: "LinkedIn",
+    image: "/images/angular_simple.svg",
+    link: "https://brand.linkedin.com/downloads",
+  },
+  {
+    name: "GitHub",
+    image: "/images/css3.svg",
+    link: "https://github.com/logos",
+  },
+  {
+    name: "Instagram",
+    image: "/images/less.svg",
+    link: "https://en.instagram-brand.com/assets/icons",
+  },
+];
+
 const SocialLink = ({ image, name, link }) => (
   <div>
     <a href={link} target="_blank" rel="noreferrer">
@@ -15,27 +33,19 @@ const SocialLink = ({ image, name, link }) => (
   </div>
 );
 
-const AboutMe = () => {
-  const social = {
-    id: Math.random(),
-    image: "https://placecorgi.com/50",
-    name: "social 1",
-    link: "https://placecorgi.com/50",
-  };
-  const list = new Array(5).fill(social);
-  return (
-    <div>
-      <h2 className="text-4xl">Contatti</h2>
-      <div className="flex">
-        <ContactForm />
-        <div>
-          {list.map(({ id, image, name, link }) => (
-            <SocialLink key={id} image={image} name={name} link={link} />
-          ))}
-        </div>
+const AboutMe = () => (
+  <div>
+    <h2 className="text-4xl">Contatti</h2>
+    <div className="flex">
+      <ContactForm />
+      <div>
+        {list.map(({ image, name, link }) => {
+          const id = Math.random().toString(36).slice(2);
+          return <SocialLink key={id} image={image} name={name} link={link} />;
+        })}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default AboutMe;
