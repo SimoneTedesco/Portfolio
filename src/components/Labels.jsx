6672f94c68ff5b254f30bb85f2f4167b81126a4f
� Simone <simonetedesco.dev@gmail.com>
2021-01-07 23:17:14 +0100

    test for framer-motion

--- src/components/Labels.jsx
+++ src/components/Labels.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
 import PropTypes from "prop-types";
+import { motion } from "framer-motion";
 
 const Label = ({ color, icon, name }) => (
-  <div
+  <motion.div
+    whileHover={{ scale: 1.1 }}
     style={{ backgroundColor: color }}
     className="flex rounded-full mb-4 mr-4 py-2 px-4 flex-label md:flex-initial md:flex-grow"
   >
@@ -15,7 +17,7 @@ const Label = ({ color, icon, name }) => (
       height="40px"
     />
     <span className="ml-2 my-auto">{name}</span>
-  </div>
+  </motion.div>
 );
 
 const Labels = () => (
@@ -37,7 +39,7 @@ const Labels = () => (
       }
     `}
     render={(data) => (
-      <section className="flex flex-wrap my-16">
+      <section className="flex flex-wrap my-64">
         {data.allMarkdownRemark.edges.map(({ node }) => {
           const { color, icon, name } = node.frontmatter;
           return <Label key={node.id} color={color} icon={icon} name={name} />;
