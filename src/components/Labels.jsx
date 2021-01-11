66d28d3435f728c8d97ded7e40757bf0252e0f6d
� Simone <simonetedesco.dev@gmail.com>
2021-01-11 23:27:49 +0100

    updated techstack view

--- src/components/Labels.jsx
+++ src/components/Labels.jsx
@@ -4,20 +4,20 @@ import PropTypes from "prop-types";
 import { motion } from "framer-motion";
 
 const Label = ({ color, icon, name }) => (
-  <motion.div
-    whileHover={{ scale: 1.1 }}
-    style={{ backgroundColor: color }}
-    className="flex rounded-full h-12 mb-4 mr-4 py-2 px-4 flex-label md:flex-initial md:flex-grow"
+  <div
+  // whileHover={{ scale: 1.1 }}
+  // style={{ backgroundColor: color }}
+  // className="flex rounded-full h-12 mb-4 mr-4 py-2 px-4 flex-label md:flex-initial md:flex-grow"
   >
     <img
       src={icon}
       alt={name}
-      className="rounded-full"
+      className="rounded-full m-auto"
       width="40px"
       height="40px"
     />
-    <span className="ml-2 my-auto">{name}</span>
-  </motion.div>
+    <span className="my-auto">{name}</span>
+  </div>
 );
 
 const Labels = () => (
@@ -40,11 +40,35 @@ const Labels = () => (
     `}
     render={(data) => (
       // <section className="flex flex-wrap my-64">
-      <section className="flex flex-wrap items-center h-screen w-full p-32 bg-green-300">
-        {data.allMarkdownRemark.edges.map(({ node }) => {
-          const { color, icon, name } = node.frontmatter;
-          return <Label key={node.id} color={color} icon={icon} name={name} />;
-        })}
+      <section className="h-screen w-full p-32 bg-green-300">
+        <h2 className="text-4xl mb-4">Skills &amp; Tools</h2>
+        <div className="flex justify-between">
+          {data.allMarkdownRemark.edges.map(({ node }) => {
+            const { color, icon, name } = node.frontmatter;
+            return (
+              <Label key={node.id} color={color} icon={icon} name={name} />
+            );
+          })}
+        </div>
+        <h2 className="text-4xl mb-4">Other tools</h2>
+        <div className="flex justify-between">
+          {data.allMarkdownRemark.edges.map(({ node }) => {
+            const { color, icon, name } = node.frontmatter;
+            return (
+              <Label key={node.id} color={color} icon={icon} name={name} />
+            );
+          })}
+        </div>
+        <h2 className="text-4xl mb-4">Stuff I&apos;d like to learn</h2>
+        {/* node.js, react native, svelte */}
+        <div className="flex justify-between">
+          {data.allMarkdownRemark.edges.map(({ node }) => {
+            const { color, icon, name } = node.frontmatter;
+            return (
+              <Label key={node.id} color={color} icon={icon} name={name} />
+            );
+          })}
+        </div>
       </section>
     )}
   />
