84a83349252477311ec21b946e32abb4697e7a37
� Simone <simonetedesco.dev@gmail.com>
2021-01-15 23:15:43 +0100

    added focus trap

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,22 +3,23 @@ import { StaticQuery, graphql } from "gatsby";
 import PropTypes from "prop-types";
 import ScrollReveal from "scrollreveal";
 import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import FocusTrap from "focus-trap-react";
 import TechStack from "./TechStack";
 
 const ProjectCard = ({ id, image, name, techStack, __html, setShowModal }) => {
   const [selectedId, setSelectedId] = useState(null);
   const modalRef = useRef(null);
   const closeModalEsc = (e) => {
-    if (e.key === 'Escape') {
+    if (e.key === "Escape") {
       setShowModal(false);
     }
-  }
-  
-  useEffect(() => {
-    if (selectedId !== null && modalRef.current) {
-      modalRef.current.focus();
-    }
-  }, [selectedId])
+  };
+
+  // useEffect(() => {
+  //   if (selectedId !== null && modalRef.current) {
+  //     modalRef.current.focus();
+  //   }
+  // }, [selectedId]);
 
   return (
     <>
@@ -42,96 +43,100 @@ const ProjectCard = ({ id, image, name, techStack, __html, setShowModal }) => {
       </motion.div>
       <AnimatePresence exitBeforeEnter>
         {selectedId && (
-          <motion.div
-            className="p-8 bg-gray-500 rounded overlay"
-            layoutId={selectedId}
-            onClick={() => setSelectedId(null)}
-            onKeyDown={closeModalEsc}
-            tabIndex={0}
-            ref={modalRef}
-          >
-            <motion.button onClick={() => setSelectedId(null)}>x</motion.button>
+          // https://gist.github.com/myogeshchavan97/d50d42aa9205573b811587d57c2e58a6#file-trap_focus-js
+          <FocusTrap>
             <motion.div
-              style={{
-                backgroundImage: `url(${image}?nf_resize=fit&w=250)`,
-                height: "200px",
-              }}
-              className="bg-transparent bg-cover bg-no-repeat bg-top cover-transition hover:bg-bottom transform hover:scale-110"
-            />
-            <motion.h3 className="text-2xl">{name}</motion.h3>
-            <TechStack list={techStack} />
-            <motion.div dangerouslySetInnerHTML={{ __html }} />
-          </motion.div>
+              className="p-8 bg-gray-500 rounded overlay"
+              layoutId={selectedId}
+              onClick={() => setSelectedId(null)}
+              onKeyDown={closeModalEsc}
+              tabIndex={0}
+              ref={modalRef}
+            >
+              <motion.button onClick={() => setSelectedId(null)}>
+                x
+              </motion.button>
+              <motion.div
+                style={{
+                  backgroundImage: `url(${image}?nf_resize=fit&w=250)`,
+                  height: "200px",
+                }}
+                className="bg-transparent bg-cover bg-no-repeat bg-top cover-transition hover:bg-bottom transform hover:scale-110"
+              />
+              <motion.h3 className="text-2xl">{name}</motion.h3>
+              <TechStack list={techStack} />
+              <motion.div dangerouslySetInnerHTML={{ __html }} />
+            </motion.div>
+          </FocusTrap>
         )}
       </AnimatePresence>
     </>
   );
 };
 
-const Projects = ({ setShowModal }) => 
+const Projects = ({ setShowModal }) => (
   // useEffect(() => {
   //   setInterval(() => {
   //     setShowModal(true);
   //   }, 4000);
   // }, [setShowModal]);
-   (
-    // useEffect(() => {
-    //   ScrollReveal().reveal(".grid.grid-cols-1 div.p-8:nth-child(odd)", {
-    //     interval: 200,
-    //     // reset: true,
-    //   });
-    //   ScrollReveal().reveal(".grid.grid-cols-1 div.p-8:nth-child(even)", {
-    //     interval: 200,
-    //     // reset: true,
-    //     delay: 400,
-    //   });
-    // }, []);
+  // useEffect(() => {
+  //   ScrollReveal().reveal(".grid.grid-cols-1 div.p-8:nth-child(odd)", {
+  //     interval: 200,
+  //     // reset: true,
+  //   });
+  //   ScrollReveal().reveal(".grid.grid-cols-1 div.p-8:nth-child(even)", {
+  //     interval: 200,
+  //     // reset: true,
+  //     delay: 400,
+  //   });
+  // }, []);
 
-    <StaticQuery
-      query={graphql`
-        query getAllProjects {
-          allMarkdownRemark(
-            filter: { fileAbsolutePath: { regex: "/projects/" } }
-            sort: { fields: frontmatter___startDate, order: DESC }
-            limit: 8
-          ) {
-            edges {
-              node {
-                id
-                html
-                frontmatter {
-                  image
-                  name
-                  startDate
-                  techStack
-                  endDate
-                }
+  <StaticQuery
+    query={graphql`
+      query getAllProjects {
+        allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex: "/projects/" } }
+          sort: { fields: frontmatter___startDate, order: DESC }
+          limit: 8
+        ) {
+          edges {
+            node {
+              id
+              html
+              frontmatter {
+                image
+                name
+                startDate
+                techStack
+                endDate
               }
             }
           }
         }
-      `}
-      render={(data) => (
-        <section className="h-full w-full p-32 bg-red-400">
-          <h2 className="text-4xl mb-4">Projects</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-16">
-            <AnimateSharedLayout type="crossfade">
-              {data.allMarkdownRemark.edges.map(({ node }) => {
-                const { image, name, techStack } = node.frontmatter;
-                return (
-                  <ProjectCard
-                    key={node.id}
-                    id={node.id}
-                    image={image}
-                    name={name}
-                    techStack={techStack}
-                    __html={node.html}
-                    setShowModal={setShowModal}
-                  />
-                );
-              })}
-            </AnimateSharedLayout>
-            {/* {data.allMarkdownRemark.edges.map(({ node }) => {
+      }
+    `}
+    render={(data) => (
+      <section className="h-full w-full p-32 bg-red-400">
+        <h2 className="text-4xl mb-4">Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-16">
+          <AnimateSharedLayout type="crossfade">
+            {data.allMarkdownRemark.edges.map(({ node }) => {
+              const { image, name, techStack } = node.frontmatter;
+              return (
+                <ProjectCard
+                  key={node.id}
+                  id={node.id}
+                  image={image}
+                  name={name}
+                  techStack={techStack}
+                  __html={node.html}
+                  setShowModal={setShowModal}
+                />
+              );
+            })}
+          </AnimateSharedLayout>
+          {/* {data.allMarkdownRemark.edges.map(({ node }) => {
             const { image, name, techStack } = node.frontmatter;
             return (
               <ProjectCard
@@ -143,13 +148,11 @@ const Projects = ({ setShowModal }) =>
               />
             );
           })} */}
-          </div>
-        </section>
-      )}
-    />
-  )
-;
-
+        </div>
+      </section>
+    )}
+  />
+);
 export default Projects;
 
 ProjectCard.propTypes = {
