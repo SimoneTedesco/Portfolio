53284c599a1372ea5e0cab6f9115d4c5bb5cab9d
� Simone <simonetedesco.dev@gmail.com>
2021-01-30 23:22:05 +0100

    added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: "var(--rich-black)",
+      secondary: "var(--baby-powder)",
+      accent: "var(--tiffany-blue)",
+      accent1: "var(--middle-blue-green)",
+      accent2: "var(--viridian-green)",
+    },
   },
   variants: {
     extend: {
