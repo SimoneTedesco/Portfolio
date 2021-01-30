f08d3433fddbb15ecc0b29a8d316f8c3b405bce4
� Simone <simonetedesco.dev@gmail.com>
2021-01-30 23:27:16 +0100

    extend instead of override

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      primary: "var(--rich-black)",
-      secondary: "var(--baby-powder)",
-      accent: "var(--tiffany-blue)",
-      accent1: "var(--middle-blue-green)",
-      accent2: "var(--viridian-green)",
+    extend: {
+      colors: {
+        primary: "var(--rich-black)",
+        secondary: "var(--baby-powder)",
+        accent: "var(--tiffany-blue)",
+        accent1: "var(--middle-blue-green)",
+        accent2: "var(--viridian-green)",
+      },
     },
   },
   variants: {
